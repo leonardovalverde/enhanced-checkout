9a76f3ad32896ecbab49a771a953507892298792
Valverde <leovmsi@hotmail.com>
2023-06-17 00:32:05 -0300

    feat: create ui tokens

---
+++ src/ui-tokens/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  text: "#151516",
+  primary: "#191847",
+  secondary: "#F5850B",
+  grey50: "#F4F3F6",
+  grey100: "#E1DEE8",
+  error: "#d32f2f",
+};
+
+export { colors };
---
+++ src/ui-tokens/fontSize.ts
@@ -0,0 +1,24 @@
+const fontSize = {
+  h4: {
+    size: "20px",
+    lineHeight: "26px",
+  },
+  body: {
+    size: "16px",
+    lineHeight: "20px",
+  },
+  bodyAlternative: {
+    size: "14px",
+    lineHeight: "18px",
+  },
+  footNote: {
+    size: "12px",
+    lineHeight: "16px",
+  },
+  exception: {
+    size: "10px",
+    lineHeight: "13px",
+  },
+};
+
+export { fontSize };
